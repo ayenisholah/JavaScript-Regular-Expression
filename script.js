3d1254ee89be8fa9235ab782c0b828309acf1222
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-02 13:31:12 +0100

    matching strings

---
+++ script.js
@@ -0,0 +1,10 @@
+let result;
+
+// let mySentence = "The dog chased the cat";
+// let regex = /The/;
+
+// let myString = "Hello World";
+// let myRegex = /Hello/;
+
+// result = myRegex.test(myString);
+// console.log(result);
