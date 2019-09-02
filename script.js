acb2983850a2fa075a89c6effde7dd12728c8342
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-02 13:51:36 +0100

    extracting a word from a given string

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let result;
+let myString;
+let myRegex;
 
 // let mySentence = "The dog chased the cat";
 // let regex = /The/;
@@ -11,5 +13,15 @@ let result;
 // let petString = "James has a pet cat";
 // let petRegex = /dog | cat| bird | fish/
 // result = petRegex.test(petString);
+
+// myString = "freeCodeCamp";
+// let fccRegex = /freecodecamp/i
+
+// result = fccRegex.test(myString)
+
+// myString = "Extract the word 'coding' from this string"
+// myRegex = /coding/
+// result = myString.match(myRegex);
+
 
 console.log(result);
